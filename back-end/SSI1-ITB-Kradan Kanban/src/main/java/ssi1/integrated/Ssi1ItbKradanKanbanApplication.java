9ff7d4feedb811927dc63a85305a17c1ec237bc9
bisbat <baitong.sae-@mail.kmutt.ac.th>
2024-04-23 15:10:45 +0700

    backend starter project

---
+++ back-end/SSI1-ITB-Kradan Kanban/src/main/java/ssi1/integrated/Ssi1ItbKradanKanbanApplication.java
@@ -0,0 +1,13 @@
+package ssi1.integrated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssi1ItbKradanKanbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssi1ItbKradanKanbanApplication.class, args);
+	}
+
+}
---
+++ back-end/SSI1-ITB-Kradan Kanban/src/test/java/ssi1/integrated/Ssi1ItbKradanKanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package ssi1.integrated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssi1ItbKradanKanbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

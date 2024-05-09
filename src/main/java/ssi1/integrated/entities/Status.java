97cff4efe5850a5bdeb11a3402c2acd57ca28a92
mg.apisara <111364885+mg-dotcom@users.noreply.github.com>
2024-05-10 04:11:02 +0700

    add time edit and create

--- src/main/java/ssi1/integrated/entities/Status.java
+++ src/main/java/ssi1/integrated/entities/Status.java
@@ -6,8 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import ssi1.integrated.dtos.TaskDTO;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @Getter
@@ -24,6 +27,12 @@ public class Status {
     @Column(name = "statusDescription")
     private String description;
     private String statusColor;
+    @CreationTimestamp
+    @Column(name="createdOn",  nullable = false, updatable = false ,insertable = false )
+    private ZonedDateTime createdOn;
+    @UpdateTimestamp
+    @Column(name="updatedOn" ,nullable = false,insertable = false)
+    private ZonedDateTime updatedOn;
     @JsonIgnore
     @OneToMany(mappedBy="status")
     private List<Task> tasks;

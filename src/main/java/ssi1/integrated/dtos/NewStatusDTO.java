8d36584529a3421c1ed38a1536149333a780e9f1
mg.apisara <111364885+mg-dotcom@users.noreply.github.com>
2024-05-13 22:39:23 +0700

    Update NewStatusDTO.java

--- src/main/java/ssi1/integrated/dtos/NewStatusDTO.java
+++ src/main/java/ssi1/integrated/dtos/NewStatusDTO.java
@@ -22,7 +22,6 @@ public class NewStatusDTO {
     private String description;
     private String statusColor;
 
-<<<<<<< HEAD
 
     public void setName(String title){
         this.name = title.trim();
@@ -33,20 +32,6 @@ public class NewStatusDTO {
     }
 
     public void setStatusColor(String statusColor){
-        this.statusColor = (statusColor != null) ? statusColor.trim() : statusColor;
-    }
-}
-=======
-    public void setName(String name){
-        this.name = name.trim();
-    }
-
-    public void setDescription(String description){
-        this.description =  (description != null) ? description.trim() : null;
-    }
-
-    public void setStatusColor(String statusColor){
         this.statusColor = (statusColor != null) ? statusColor.trim() : "#CCCCC";
     }
 }
->>>>>>> 84e22339caba39484031051a26d0cde2485e0397

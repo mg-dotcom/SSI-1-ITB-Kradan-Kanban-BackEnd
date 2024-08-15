17396e3251ff9bd1327c67a6e0fd3cbaac02f083
bisbat <baitong.sae-@mail.kmutt.ac.th>
2024-08-15 21:51:41 +0700

    start argon2

---
+++ src/main/java/ssi1/integrated/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package ssi1.integrated.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ssi1.integrated.user_account.UserRepository;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final UserRepository userRepository;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new Argon2PasswordEncoder(9,16,1,16,2);
+    }
+}

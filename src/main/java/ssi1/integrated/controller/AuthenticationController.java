4f808d0521a029df249179a9572ca00219bc8fad
bisbat <baitong.sae-@mail.kmutt.ac.th>
2024-08-19 12:51:09 +0700

    Done pbi 15

--- src/main/java/ssi1/integrated/controller/AuthenticationController.java
+++ src/main/java/ssi1/integrated/controller/AuthenticationController.java
@@ -1,39 +1,27 @@
 package ssi1.integrated.controller;
 
-        import lombok.RequiredArgsConstructor;
         import org.springframework.http.HttpStatus;
         import org.springframework.http.ResponseEntity;
-        import org.springframework.security.authentication.AuthenticationManager;
-        import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-        import org.springframework.security.core.Authentication;
-        import org.springframework.security.core.context.SecurityContextHolder;
         import org.springframework.web.bind.annotation.PostMapping;
         import org.springframework.web.bind.annotation.RequestBody;
         import org.springframework.web.bind.annotation.RequestMapping;
         import org.springframework.web.bind.annotation.RestController;
         import ssi1.integrated.security.AuthenticationService;
         import ssi1.integrated.security.dtos.AuthenticationRequest;
-        import ssi1.integrated.security.dtos.AuthenticationResponse;
 
 @RestController
 @RequestMapping("/api/auth")
-//@RequiredArgsConstructor
 public class AuthenticationController {
-//    private final AuthenticationService service;
-    private final AuthenticationManager authenticationManager;
-
-    public AuthenticationController(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
+    private final AuthenticationService service;
+    public AuthenticationController(AuthenticationService service) {
+        this.service = service;
     }
 
     @PostMapping("/login")
     public ResponseEntity<String> authenticate(
             @RequestBody AuthenticationRequest request
     ) {
-        Authentication authentication=authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getUsername(),request.getPassword()
-        ));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+        service.authenticate(request);
         return new ResponseEntity<>("User login successful", HttpStatus.OK);
     }
 }
--- src/main/java/ssi1/integrated/security/AuthenticationService.java
+++ src/main/java/ssi1/integrated/security/AuthenticationService.java
@@ -3,28 +3,21 @@ package ssi1.integrated.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import ssi1.integrated.security.dtos.AuthenticationRequest;
-import ssi1.integrated.security.dtos.AuthenticationResponse;
-import ssi1.integrated.user_account.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
-    private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
-    private final PasswordEncoder passwordEncoder;
+    public void authenticate(AuthenticationRequest request){
+        Authentication authentication= authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(),request.getPassword())
 
-    public Object authenticate(AuthenticationRequest request){
-        System.out.println("Before auth");
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getUsername(),
-                        request.getPassword()
-                )
         );
-        System.out.println("After auth");
-        return new Object();
-    }
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+}
 }

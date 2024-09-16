ce980a53c3120e27ed938a14fdf7c413e8bd850f
Tew2945 <110712512+Tew2945@users.noreply.github.com>
2024-09-16 15:47:12 +0700

    fix get task by Id

--- src/main/java/ssi1/integrated/services/TaskService.java
+++ src/main/java/ssi1/integrated/services/TaskService.java
@@ -52,17 +52,17 @@ public class TaskService {
 
 //bi
     public Task getTaskById(Integer taskId, String boardId) {
-        Task task = taskRepository.findByStatusIdAndBoardId(taskId, boardId);
+        Task task = taskRepository.findByIdAndBoardId(taskId, boardId);
         task.setStatus(task.getStatus());
         return task;
     }
 //Tew
-    // public TaskDTO getTaskById(Integer taskId, String boardId) {
-    //     Task task = taskRepository.findByIdAndBoardId(taskId, boardId);
-    //     TaskDTO taskDTO = modelMapper.map(task, TaskDTO.class);
-    //     taskDTO.setStatus(task.getStatus().getName());
-    //     return taskDTO;
-    // }
+//     public TaskDTO getTaskById(Integer taskId, String boardId) {
+//         Task task = taskRepository.findByIdAndBoardId(taskId, boardId);
+//         TaskDTO taskDTO = modelMapper.map(task, TaskDTO.class);
+//         taskDTO.setStatus(task.getStatus().getName());
+//         return taskDTO;
+//     }
 
     @Transactional
     public GeneralTaskDTO insertNewTask(NewTaskDTO newTask, String boardId) {
